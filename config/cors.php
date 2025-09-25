579fb3377e433196dcac7ac0f0ca84580854116f
honghav <158126590+honghav@users.noreply.github.com>
2025-09-25 11:00:49 +0700

    Back end connect front-end1

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // your frontend
+    'allowed_origins' => [
+        'http://localhost:3000',         // local dev
+        'https://j-pich-front-5for-3bh78tcn9-honghavs-projects.vercel.app', // your deployed frontend
+    ],
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
 ];
