f27d261435f21338c0c63e1b0b5c8a9e6038d205
honghav <158126590+honghav@users.noreply.github.com>
2025-09-27 13:29:38 +0700

    Fixing From Front End version2

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,8 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',         // local dev
-        'https://j-pich-front-5for-3bh78tcn9-honghavs-projects.vercel.app', // your deployed frontend
+        'https://j-pich-front-5for-3bh78tcn9-honghavs-projects.vercel.app', // version 1
+        'https://j-pich-front-5for-eqkvq8tgd-honghavs-projects.vercel.app', // version 2
     ],
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
