d549855456fadfdf8cbe30de1ce9cd211208b04e
honghav <158126590+honghav@users.noreply.github.com>
2025-09-30 16:56:23 +0700

    Connect back with fornt modify on config/cors

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
         'http://localhost:3000',         // local dev
         'https://j-pich-front-5for-3bh78tcn9-honghavs-projects.vercel.app', // version 1
         'https://j-pich-front-5for-eqkvq8tgd-honghavs-projects.vercel.app', // version 2
+        'https://j-pich-front-5for-4bp90bui1-honghavs-projects.vercel.app', // Version Hpanel
     ],
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
